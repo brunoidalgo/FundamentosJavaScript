237fa773d960c113b6bde4c6e189f37dde232a52
brunoidalgo <idalgob99@gmail.com>
2023-04-20 11:34:20 -0300

    Conversões

---
+++ conversoes/conversao.js
@@ -0,0 +1,15 @@
+// Tipos de conversoes
+
+// Convrsão Implícita
+
+const numero = 456;
+const numeroString = Number("456");
+
+//Number() --> Usado para transformar uma variável de qualquer tipo em Número
+//String() --> Usado para transformar uma variável de qualquer tipo em String.
+
+console.log (numero + numeroString);
+console.log(numero - Number(numeroString));
+
+// Conversão Explícita
+
