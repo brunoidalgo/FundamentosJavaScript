93cf388dea218d6b9efb2f01c6b02a293ab33add
brunoidalgo <idalgob99@gmail.com>
2023-04-26 10:53:40 -0300

    Ternário e Template-String

---
+++ comparacoes/ternario.js
@@ -0,0 +1,16 @@
+// Operador ternário 
+
+const idadeMinima = 18;
+idadeCliente = 19;
+
+if (idadeCliente >= idadeMinima) {
+    console.log('cerveja');
+} else {
+    console.log('suco');
+}
+
+
+// Operador Ternário
+// --------- condição                      //true    //false
+console.log(idadeCliente >= idadeMinima ? 'cerveja': 'suco' )
+
---
+++ tipos/template-string.js
@@ -0,0 +1,13 @@
+// Template String
+
+const nome = "Bruno"
+const idade = 2023 - 2002;
+const cidade = "Ribeirão Preto"
+
+const resultado = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidade;
+
+console.log(resultado);
+
+const templateString = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade}`;
+
+console.log(templateString);
