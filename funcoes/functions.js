2a7f16631d04d800bcf1fa2cf98eed275172cc8a
brunoidalgo <idalgob99@gmail.com>
2023-04-26 13:16:58 -0300

    Funções

---
+++ funcoes/functions.js
@@ -0,0 +1,23 @@
+// Funções 
+
+// 1) Declara função
+
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+// 2) Executa a função (1 ou + vezes)
+
+imprimeTexto('Oi');
+imprimeTexto('Bruno')
+
+// Três modos de escrever funções
+
+function soma() {
+    return 2 + 2;
+}
+
+
+console.log(soma());
+imprimeTexto(soma());
+
